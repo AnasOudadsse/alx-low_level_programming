d53778359f7991c2d1c0e5f6e634afa3587647bc
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-06 05:16:36 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	#ifdef __x86_64__
-	printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
-	#else
-	printf("Size of a double: %zu byte(s)\n", sizeof(double));
-	#endif
 	return (0);
 }
