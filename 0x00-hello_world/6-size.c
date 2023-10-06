de0a6bf25477ea9fe63bd1d78a8dc20e3fe95ff7
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-05 20:23:15 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdint.h>
 /**
  * main - Entry point
  *
@@ -7,17 +6,13 @@
  */
 int main(void)
 {
-#if __WORDSIZE == 64
-	printf("64-bit system\n");
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
-#else
-	printf("32-bit system\n");
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
-#endif
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a double: %zu byte(s)\n", sizeof(double));
+	printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 
 	return (0);
 }
