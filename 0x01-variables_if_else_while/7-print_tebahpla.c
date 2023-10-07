1e7da87682b5ca461013568c112001e05a2d360c
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-06 17:20:06 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letters = 'z';
+
+	while (letters >= 'a')
+{
+		putchar(letters);
+		letters++;
+}
+	putchar('\n');
+	return (0);
+}
