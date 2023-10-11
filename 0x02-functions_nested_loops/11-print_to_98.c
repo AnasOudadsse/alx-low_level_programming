648ed714339d24e6280541ef695d8513c7224b1f
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-11 09:28:06 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%2d, ", n);
+			printf("%d, ", n);
 			n++;
 		}
 		printf("%2d\n", 98);
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%2d, ", n);
+			printf("%d, ", n);
 			n--;
 		}
 		printf("%2d\n", 98);
