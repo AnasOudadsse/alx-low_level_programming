cd2c57d53892b8b3be8cb1e5d7307cadb7e1b3df
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-10 17:33:29 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isalpha - checks if c is a letter
+* @c: the character to check
+* Return: 1 if true and 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	return (isalpha(c));
+}
