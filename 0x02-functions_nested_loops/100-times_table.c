04590b16bda48b0f70856bb40b438ed4ac1e6359
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-11 10:58:44 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,25 +9,21 @@
 
 void print_times_table(int n)
 {
-
 	int i;
 	int j;
 	int product;
 
 	if (n < 0 || n > 15)
-	{
-	return;
-	}
+		return;
 	if (n == 0)
 		printf("%d", n);
-	else 
+	else
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				product = i * j;
-
 				if (j == 0)
 					printf("%d,", product);
 				else if (j == n)
@@ -35,7 +31,6 @@ void print_times_table(int n)
 				else
 					printf("%4d,", product);
 			}
-		
 		printf("\n");
 		}
 	}
