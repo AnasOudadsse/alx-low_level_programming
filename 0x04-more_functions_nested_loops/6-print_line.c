42e34368e2278c8d03349b44dde07dee7ca9a6c9
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-12 16:43:54 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,12 @@
 
 void print_line(int n)
 {
-	int i = 1;
+	int i = 0;
 
-	if (n == 0)
-		_putchar('\n');
-	else
+	while (i < n)
 	{
-		while (i < n)
-		{
-			_putchar('_');
-			i++;
-		}
+		_putchar('_');
+		i++;
 	}
 	_putchar('\n');
 }
