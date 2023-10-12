3ff94a5001224caa1b7f1a9a8143df5409ac6362
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-12 14:05:47 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - multiply two ints
+* @a: first param
+* @b: second param
+* Return: return a * b
+*/
+
+int mul(int a, int b)
+{
+	int p = a * b;
+
+	return (p);
+}
