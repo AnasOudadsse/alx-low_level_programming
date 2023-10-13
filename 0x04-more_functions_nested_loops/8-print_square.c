a40ca6a2513e6c65fd4bd69ff4e0db49b2a7f0f2
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-12 17:49:17 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,19 @@ void print_square(int size)
 	int i;
 	int j;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < (size - 1); j++)
 				_putchar('#');
+
+			_putchar('#');
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
