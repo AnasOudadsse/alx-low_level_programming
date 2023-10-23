418a182ca09a0713796456f0f94f4f4d0803b9a9
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-23 11:16:41 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/*
+/**
  * _memset - fills the memory pointed by s with the constant b n times
  * @s: the pointer where the b will be stocked
  * @b: the constant that will be stocked
  * @n: the number of b that will be stocked
+ * Return: pointer to s
 */
 
 
