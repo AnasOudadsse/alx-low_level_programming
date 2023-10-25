895b70fa202d7bcb1d814391a30802a68d0c3dae
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-10-25 14:17:27 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - calculates the length of a string using recursion
+ * @s: the string to calculate the length of
+ * 
+ * Return: the length of the string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -7,5 +13,5 @@ int _strlen_recursion(char *s)
 		return (0);
 	}
 
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
